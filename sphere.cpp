79724645adc4e5035ad3c77c81ceac47191c1177
Matthew Clayton <claytomp@mail.uc.edu>
2016-10-24 19:48:49 -0400

    Creation
    Creation of initial file

---
+++ sphere.cpp
@@ -0,0 +1,9 @@
+/*
+* @author Matthew Clayton
+* @date 24 October 2016
+* @assign.ment H5 Class Practice: Classes and Functions
+* @descrip.tion
+* This file contains the first class of the H5 assignment for EECE1080C.
+* This file contains a solution to finding the surface area and volume of a
+* sphere.
+*/
